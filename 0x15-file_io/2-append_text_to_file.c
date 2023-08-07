a13a65f6634c4d56ad5cd50bd2c52286c2f086af
alidexter001 <dexter.ali001@gmail.com>
2023-08-07 05:13:05 -0700

    3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,11 +5,12 @@
  * @filename: The name of the file.
  * @text_content: content to add at the end of the file.
  *
- * Return: fails - -1.
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int a, b, len = 0;
+	int b, w, len = 0;
+
 	if (filename == NULL)
 		return (-1);
 
@@ -18,11 +19,14 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	a = open(filename, O_WRONLY | O_APPEND);
-	b = write(a, text_content, len);
-	if (a == -1 || b == -1)
+
+	b = open(filename, O_WRONLY | O_APPEND);
+	w = write(b, text_content, len);
+
+	if (b == -1 || w == -1)
 		return (-1);
 
-	close(a);
+	close(b);
+
 	return (1);
 }
