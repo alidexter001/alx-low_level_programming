62217743ce00c027648596a5bb8a9cf1f6664b56
alidexter001 <dexter.ali001@gmail.com>
2023-08-07 04:17:53 -0700

    0x15-file_io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,8 +16,8 @@ int file_create(const char *filename, char *text_content);
 int append_my_text_to_file(const char *filename, char *text_content);
 
 /*
-ssize_t: Used for a count of bytes or an error indication.
-int: initialisation
+* ssize_t: Used for a count of bytes or an error indication.
+* int: initialisation
 */
 
 
