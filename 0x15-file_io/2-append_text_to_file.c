a25d7c68393718a5e7f33a89df839e596af9db56
alidexter001 <dexter.ali001@gmail.com>
2023-08-07 05:15:33 -0700

    3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,7 +2,7 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int b, w, len = 0;
+	int o, w, len = 0;
 
 
 	if (filename == NULL)
@@ -16,15 +16,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 
-	b = open(filename, O_WRONLY | O_APPEND);
-	w = write(b, text_content, len);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
 
-	if (b == -1 || w == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
 
-	close(b);
+	close(o);
 
 
 	return (1);
