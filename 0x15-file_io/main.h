1b5b38a020374ba0c414cace3e30cb13501a47f0
alidexter001 <dexter.ali001@gmail.com>
2023-08-07 04:53:48 -0700

    Fixing

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,26 +1,22 @@
-#ifndef HEADER
-#define HEADER
-/*
- * main.h icnludes read_textfile.c + create_file.c + append_text_to_file.c
-*/
 
 
 
+#ifndef MAIN_H
+#define MAIN_H
+
+/*Includes*/
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 
-
-
-//Code for main function
-int _putchar(char c);
+/*Code Section*/
 ssize_t read_textfile(const char *filename, size_t letters);
+
 int create_file(const char *filename, char *text_content);
+
 int append_text_to_file(const char *filename, char *text_content);
 
+#endif
 
-
-
-#endif /* MAIN_H */
